b799495c82860b7e18e729abde1f4de317692ea3
LAB23_USER ON WS24 <L23Y15W24@LabHCI.net>
2025-10-28 16:23:07 +0800

    Fix STATIC_ROOT path issue for Render

--- SmartBus/SmartBus/settings.py
+++ SmartBus/SmartBus/settings.py
@@ -77,7 +77,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR, "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
